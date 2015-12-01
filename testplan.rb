172e5d9e2aa24d604213b3452d52439be72f80dc
John Hawthorn <john.hawthorn@gmail.com>
2015-12-01 11:54:34 -0800

    Split SolidusDriver from RailsDriver

--- testplan.rb
+++ testplan.rb
@@ -1,6 +1,48 @@
 require 'ruby-jmeter'
 
-class SolidusDriver < SimpleDelegator
+class RailsDriver < SimpleDelegator
+  private
+
+  def put(url, params={}, options={}, &block)
+    post(url, {'_method' => 'put'}.merge(params), options, &block)
+  end
+
+  def patch(url, params={}, options={}, &block)
+    post(url, {'_method' => 'patch'}.merge(params), options, &block)
+  end
+
+  def post(url, params={}, options={}, &block)
+    block ||= ->{}
+    params = params.merge('authenticity_token' => '${__urlencode(${authenticity_token})}')
+    options = options.merge(fill_in: Hash[to_query(params)])
+    submit(url, options) do
+      instance_exec(&block)
+    end
+  end
+
+  def to_query(value, base=nil)
+    case value
+    when Hash
+      value.flat_map do |k,v|
+        to_query(v, append_key(base, k))
+      end
+    when Array
+      value.flat_map do |v|
+        to_query(v, "#{base}[]")
+      end
+    when nil
+      []
+    else
+      [[base, value]]
+    end
+  end
+
+  def append_key(base, new)
+    base ? "#{base}[#{new}]" : new
+  end
+end
+
+class SolidusDriver < RailsDriver
   def perform
     extract css: 'meta[name=csrf-token]', name: 'authenticity_token', attribute: :content
 
@@ -56,53 +98,6 @@ class SolidusDriver < SimpleDelegator
     patch '/checkout/update/confirm' do
       response_assertion contains: 'Your order has been processed successfully', scope: :parent
     end
-  end
-
-  private
-
-  def visit(*args, &block)
-    block ||= ->{}
-    super(*args) do
-      instance_exec(&block)
-    end
-  end
-
-  def put(url, params={}, options={}, &block)
-    post(url, {'_method' => 'put'}.merge(params), options, &block)
-  end
-
-  def patch(url, params={}, options={}, &block)
-    post(url, {'_method' => 'patch'}.merge(params), options, &block)
-  end
-
-  def post(url, params={}, options={}, &block)
-    block ||= ->{}
-    params = params.merge('authenticity_token' => '${__urlencode(${authenticity_token})}')
-    options = options.merge(fill_in: Hash[to_query(params)])
-    submit(url, options) do
-      instance_exec(&block)
-    end
-  end
-
-  def to_query(value, base=nil)
-    case value
-    when Hash
-      value.flat_map do |k,v|
-        to_query(v, append_key(base, k))
-      end
-    when Array
-      value.flat_map do |v|
-        to_query(v, "#{base}[]")
-      end
-    when nil
-      []
-    else
-      [[base, value]]
-    end
-  end
-
-  def append_key(base, new)
-    base ? "#{base}[#{new}]" : new
   end
 end
 
